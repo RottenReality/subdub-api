0144f70b6da6a7a14ae67c8b1319a3b2bd075e18
RottenReality <andresorregolopez@gmail.com>
2025-05-23 12:57:35 -0500

    add function to user routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import { ADMIN_ID } from '../config/env.js'
+import { signUp } from "./auth.controller.js";
 
 export const getUsers = async (req, res, next) => {
     try {
@@ -31,6 +33,89 @@ export const getUser = async (req, res, next) => {
             data: user
         })
 
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const createUser = async (req, res, next) => {
+    try {
+
+        if(req.user.id !== ADMIN_ID) {
+            const error = new Error('Access denied: admin only')
+            error.status = 403
+            throw error
+        }
+
+        signUp()
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateUser = async (req, res, next) => {
+    try {
+
+        if (req.user.id !== req.params.id && req.user.id !== ADMIN_ID) {
+            const error = new Error('Access denied: not authorized')
+            error.status = 403
+            throw error
+        }
+
+        const user = await User.findById(req.params.id)
+            if (!user) {
+            const error = new Error('User not found')
+            error.status = 404
+            throw error
+        }
+
+        const allowedUpdates = ['name', 'email', 'password']
+        const updates = Object.keys(req.body)
+        const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+
+        if (!isValidOperation) {
+            const error = new Error('Invalid updates')
+            error.status = 400
+            throw error
+        }
+
+        updates.forEach(field => {
+            user[field] = req.body[field]
+        })
+
+        await user.save()
+
+        res.status(200).json({ success: true, data: user })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    try {
+
+        const user = await User.findById(req.params.id)
+
+        if (req.user.id !== req.params.id && req.user.id !== ADMIN_ID) {
+            const error = new Error('Access denied: not authorized')
+            error.status = 403
+            throw error
+        }
+
+        if (!user) {
+            const error = new Error('User not found')
+            error.status = 404
+            throw error
+        }
+
+
+        await user.deleteOne()
+        
+        res.status(200).json({ success: true, data: `user ${req.params.id} deleted` })
+        
     } catch (error) {
         next(error)
     }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { getUser, getUsers } from "../controllers/user.controller.js";
+import { createUser, deleteUser, getUser, getUsers, updateUser } from "../controllers/user.controller.js";
 import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
 
-userRouter.get('/', getUsers)
+userRouter.get('/', authorize, getUsers)
 userRouter.get('/:id',authorize , getUser)
-userRouter.post('/', (req, res) => res.send({title: 'CREATE new users'}))
-userRouter.put('/:id', (req, res) => res.send({title: 'Update user'}))
-userRouter.delete('/:id', (req, res) => res.send({title: 'DELETE user'}))
+userRouter.post('/', authorize, createUser)
+userRouter.put('/:id', authorize, updateUser)
+userRouter.delete('/:id', authorize, deleteUser)
 
 export default userRouter
