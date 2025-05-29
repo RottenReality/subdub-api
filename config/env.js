d4327211b5fb8ebbe63e92c9a7f4abe85f786d77
RottenReality <andresorregolopez@gmail.com>
2025-05-29 17:54:51 -0500

    add workflow config

--- config/env.js
+++ config/env.js
@@ -2,4 +2,18 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_END || 'development'}.local` })
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_KEY, ARCJET_ENV, ADMIN_ID } = process.env
+export const { 
+    PORT,
+    NODE_ENV,
+    SERVER_URL, 
+    DB_URI, 
+    JWT_SECRET, 
+    JWT_EXPIRES_IN, 
+    ARCJET_KEY, 
+    ARCJET_ENV, 
+    ADMIN_ID,
+    QSTASH_URL,
+    QSTASH_TOKEN,
+    QSTASH_CURRENT_SIGNING_KEY,
+    QSTASH_NEXT_SIGNING_KEY
+} = process.env
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,5 +1,6 @@
 import Subscription from '../models/subscription.model.js'
-import { ADMIN_ID } from '../config/env.js'
+import { ADMIN_ID, SERVER_URL } from '../config/env.js'
+import { workflowClient } from '../config/upstash.js'
 
 export const getAllSubscriptions = async (req, res, next) => {
     try {
@@ -24,7 +25,18 @@ export const createSubscription = async (req, res, next) => {
             user: req.user._id,
         })
 
-        res.status(201).json({ success: true, data: subscription})
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription.id
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            retries:0,
+        })
+
+        res.status(201).json({ success: true, data: { subscription, workflowRunId }})
     } catch (error) {
         next(error)
     }
